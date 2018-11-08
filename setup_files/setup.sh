4d526b73ed3692705dd83aa28fbd710f223acf41
mgouy <mgouy@euclidanalytics.com>
2018-11-07 17:30:13 -0800

    update supervisor

--- setup_files/setup.sh
+++ setup_files/setup.sh
@@ -2,13 +2,6 @@
 # Set environment variable
 echo "AP_ID="$1 >> /etc/environment
 
-# Copy startup bash script test
-cp ./startup.sh ~/startup.sh
-cp ./initstartup /etc/init.d/
-chmod 755 /etc/init.d/initstartup
-update-rc.d initstartup defaults
-
-
 # Automated login on root 
 cp ./lightdm-autologin /etc/pam.d/lightdm-autologin
 cp ./lightdm.conf /etc/lightdm/lightdm.conf
