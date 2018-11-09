ade2ae244142078c85d499ca44a70ef5835d3dfe
mgouy <mgouy@euclidanalytics.com>
2018-11-08 17:46:51 -0800

    update mac

--- probemon.py
+++ probemon.py
@@ -17,7 +17,7 @@ from send_endpoint import send_endpoint
 
 NAME = 'probemon'
 DESCRIPTION = "a command line tool for logging 802.11 probe request frames"
-FILTERED_MAC = "f6:2f:ee:2c:c9:6c"
+FILTERED_MAC = "44:91:60:a7:5b:16"
 DEBUG = False
 ap_id = os.environ['AP_ID']
 
